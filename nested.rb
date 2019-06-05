de93bc5dcf5a41f024e4ab3b06c4cff2b883c178
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-05 23:18:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,5 +129,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages]
+     dennis_ritchies_new_languages=programmer_hash[:dennis_ritchie][:languages]
 end
